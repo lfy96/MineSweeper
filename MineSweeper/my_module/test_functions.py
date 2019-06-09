ffe4268fff56d299f032812b9e6a25898de8fe57
lfy96 <37138106+lfy96@users.noreply.github.com>
2019-06-08 20:15:05 -0700

    Add files via upload

---
+++ MineSweeper/my_module/test_functions.py
@@ -0,0 +1,20 @@
+"""Test for my functions.
+
+Note: because these are 'empty' functions (return None), here we j and return None, as expected.
+"""
+
+from functions import *
+
+##
+##
+
+def test_hitMine():
+    assert callable(hitMine)
+
+def test_isWinner():
+    assert callable(isWinner)
+
+def test_updateBoard():
+    assert callable(updateBoard)
+
+
